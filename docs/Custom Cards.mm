278ba3b7f871119a39e471a0c78873ce2309b5a8
Jonathan Dyason <jonathandyason@hotmail.com>
2018-12-31 22:21:43 +0200

    begin

---
+++ docs/Custom Cards.mm
@@ -0,0 +1,11 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1546257923012" ID="ID_1276483561" MODIFIED="1546257935258" TEXT="Custom Cards">
+<node CREATED="1546258194876" HGAP="123" ID="ID_1773483373" MODIFIED="1546275091923" POSITION="right" TEXT="user chooses card" VSHIFT="79">
+<node CREATED="1546275009790" HGAP="67" ID="ID_1007097297" MODIFIED="1546275023429" TEXT="submit photo of child" VSHIFT="-105"/>
+<node CREATED="1546275029798" HGAP="105" ID="ID_547072790" MODIFIED="1546275563636" TEXT="choose a pre-selected theme, or one of their own" VSHIFT="3"/>
+</node>
+<node CREATED="1546275140564" HGAP="103" ID="ID_509181836" MODIFIED="1546275149198" POSITION="left" TEXT="examples of cards" VSHIFT="47"/>
+<node CREATED="1546275569508" HGAP="226" ID="ID_188821648" MODIFIED="1546282943441" POSITION="right" TEXT="ordering screen" VSHIFT="157"/>
+</node>
+</map>
